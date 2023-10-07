f293d263d2ee0966224d3b5b91d24b246a53e90c
anuraag0532 <anurag.shrivastava36@gmail.com>
2023-10-07 12:59:12 +0530

    new commit

---
+++ 01basics.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "anurag@gmail.com"
+var accountPassword = " 12345"
+accountCity = "jaipur "
+//accountId = 2
+console.log(accountId)
+accountEmail="as@gmail.com"
+accountPassword="anurag"
+accountCity="satna"
+console.table(accountEmail,accountId,accountPassword,accountCity)
