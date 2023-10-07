0517ab1c0ee7c93abb714ef14f6da05221c764b5
anuraag0532 <anurag.shrivastava36@gmail.com>
2023-10-07 23:52:13 +0530

    game

--- BMICalculator/chaiaurcode.js
+++ BMICalculator/chaiaurcode.js
@@ -1,9 +1,11 @@
 const form = document.querySelector('form');
+const bmi = (weight/((height*height)/10000)).toFixed(2)
 form.addEventListener('submit', function(e){
     e.preventDefault();
     const height = parseInt(document.querySelector('#height').value)
     const weight = parseInt(document.querySelector('#weight').value)
     const results = document.querySelector('#results')
+    const messages = document.querySelector('#message')
     if(height === '' || height < 0 || isNaN(height)){
         results.innerHTML = `please give a valid height ${height}`;
     }
@@ -13,12 +15,12 @@ form.addEventListener('submit', function(e){
         const bmi = (weight/((height*height)/10000)).toFixed(2)
         results.innerHTML = `<span>${bmi}</span>`
     }
-    if(bmi<18.6){
-        results.innerHTML=<span>under weight</span>
-    }else if(bmi>18.6&&bmi<24.9){
-        results.innerHTML=<span>normal weight</span>;
+    if(bmi<14.5){
+        messages.innerHTML = "low bmi"
+    }else if(bmi>14.5&&bmi<24.5){
+        messages.innerHTML = "normal"
     }else{
-        results.innerHTML=<span>over weight</span>
-}
-  
+        messages.innerHTML = "high bmi"
+    }
+   
 });
